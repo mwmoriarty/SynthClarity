b5ec0d8ce107633a8ea811dbfc8b22172c8af56b
mworiarty <mwmoriarty@mail.com>
2026-04-22 00:25:55 -0400

    Quartz sync: Apr 22, 2026, 12:25 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -85,8 +85,7 @@ const config: QuartzConfig = {
       Plugin.Static(),
       Plugin.Favicon(),
       Plugin.NotFoundPage(),
-      // Comment out CustomOgImages to speed up build time
-      Plugin.CustomOgImages(),
+      // Plugin.CustomOgImages(),
     ],
   },
 }
