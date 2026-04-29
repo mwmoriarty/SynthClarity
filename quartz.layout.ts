a27ccc17a570c0a948a4591859ccd0f3bd99fe49
mworiarty <mwmoriarty@mail.com>
2026-04-29 14:52:08 -0400

    Quartz sync: Apr 29, 2026, 2:52 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -6,16 +6,13 @@ export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
   afterBody: [],
-  footer: Component.Footer({links: { GitHub: "https://github.com/jackyzha0/quartz", "Discord Community": "https://discord.gg/cRFFHYye7t",},}),
+  footer: Component.Footer({links: { }}),
 }
 
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.ConditionalRender({
-      component: Component.Breadcrumbs(),
-      condition: (page) => page.fileData.slug !== "index",
-    }),
+    Component.ConditionalRender({component: Component.Breadcrumbs(), condition: (page) => page.fileData.slug !== "index",}),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
@@ -23,16 +20,7 @@ export const defaultContentPageLayout: PageLayout = {
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
-    Component.Flex({
-      components: [
-        {
-          Component: Component.Search(),
-          grow: true,
-        },
-        { Component: Component.Darkmode() },
-        { Component: Component.ReaderMode() },
-      ],
-    }),
+    Component.Flex({ components: [{Component: Component.Search(), grow: true,}, {Component: Component.Darkmode()},],}),
     Component.Explorer(),
   ],
   right: [
@@ -44,19 +32,11 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta(), Component.TagList(),],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
-    Component.Flex({
-      components: [
-        {
-          Component: Component.Search(),
-          grow: true,
-        },
-        { Component: Component.Darkmode() },
-      ],
-    }),
+    Component.Flex({ components: [{Component: Component.Search(), grow: true,}, {Component: Component.Darkmode()},],}),
     Component.Explorer(),
   ],
   right: [],
